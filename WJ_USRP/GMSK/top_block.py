045bb07243917ac8995051158bd99c48245634f0
Santiago Rodriguez <siror91@gmail.com>
2016-06-07 10:52:42 -0500

    RX streaming GMSK

---
+++ WJ_USRP/GMSK/top_block.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Top Block
+# Generated: Tue Jun  7 10:27:40 2016
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from gnuradio import blocks
+from gnuradio import digital
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio import uhd
+from gnuradio import wxgui
+from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
+from gnuradio.filter import firdes
+from gnuradio.wxgui import fftsink2
+from gnuradio.wxgui import forms
+from grc_gnuradio import blks2 as grc_blks2
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import time
+import wx
+
+
+class top_block(grc_wxgui.top_block_gui):
+
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="Top Block")
+        _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
+        self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.variable_slider_0 = variable_slider_0 = 10e6
+        self.variable_slider_0_1 = variable_slider_0_1 = 10e6
+        self.samp_rate = samp_rate = variable_slider_0
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.wxgui_fftsink2_0 = fftsink2.fft_sink_c(
+        	self.GetWin(),
+        	baseband_freq=0,
+        	y_per_div=10,
+        	y_divs=10,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=1024,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title="FFT Plot",
+        	peak_hold=False,
+        )
+        self.Add(self.wxgui_fftsink2_0.win)
+        _variable_slider_0_1_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._variable_slider_0_1_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_variable_slider_0_1_sizer,
+        	value=self.variable_slider_0_1,
+        	callback=self.set_variable_slider_0_1,
+        	label='variable_slider_0_1',
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._variable_slider_0_1_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_variable_slider_0_1_sizer,
+        	value=self.variable_slider_0_1,
+        	callback=self.set_variable_slider_0_1,
+        	minimum=0,
+        	maximum=50e6,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_variable_slider_0_1_sizer)
+        _variable_slider_0_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._variable_slider_0_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_variable_slider_0_sizer,
+        	value=self.variable_slider_0,
+        	callback=self.set_variable_slider_0,
+        	label='variable_slider_0',
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._variable_slider_0_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_variable_slider_0_sizer,
+        	value=self.variable_slider_0,
+        	callback=self.set_variable_slider_0,
+        	minimum=0,
+        	maximum=20e6,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_variable_slider_0_sizer)
+        self.uhd_usrp_source_0 = uhd.usrp_source(
+        	",".join(("", "")),
+        	uhd.stream_args(
+        		cpu_format="fc32",
+        		channels=range(1),
+        	),
+        )
+        self.uhd_usrp_source_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_source_0.set_center_freq(4e9, 0)
+        self.uhd_usrp_source_0.set_gain(20, 0)
+        self.uhd_usrp_source_0.set_antenna("TX/RX", 0)
+        self.digital_gmsk_demod_0 = digital.gmsk_demod(
+        	samples_per_symbol=2,
+        	gain_mu=0.175,
+        	mu=0.5,
+        	omega_relative_limit=0.005,
+        	freq_error=0.0,
+        	verbose=False,
+        	log=False,
+        )
+        self.blocks_udp_sink_0 = blocks.udp_sink(gr.sizeof_int*1, "127.0.0.1", 1235, 1500, True)
+        self.blks2_packet_decoder_0 = grc_blks2.packet_demod_i(grc_blks2.packet_decoder(
+        		access_code="",
+        		threshold=-1,
+        		callback=lambda ok, payload: self.blks2_packet_decoder_0.recv_pkt(ok, payload),
+        	),
+        )
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blks2_packet_decoder_0, 0), (self.blocks_udp_sink_0, 0))    
+        self.connect((self.digital_gmsk_demod_0, 0), (self.blks2_packet_decoder_0, 0))    
+        self.connect((self.uhd_usrp_source_0, 0), (self.digital_gmsk_demod_0, 0))    
+        self.connect((self.uhd_usrp_source_0, 0), (self.wxgui_fftsink2_0, 0))    
+
+    def get_variable_slider_0(self):
+        return self.variable_slider_0
+
+    def set_variable_slider_0(self, variable_slider_0):
+        self.variable_slider_0 = variable_slider_0
+        self.set_samp_rate(self.variable_slider_0)
+        self._variable_slider_0_slider.set_value(self.variable_slider_0)
+        self._variable_slider_0_text_box.set_value(self.variable_slider_0)
+
+    def get_variable_slider_0_1(self):
+        return self.variable_slider_0_1
+
+    def set_variable_slider_0_1(self, variable_slider_0_1):
+        self.variable_slider_0_1 = variable_slider_0_1
+        self._variable_slider_0_1_slider.set_value(self.variable_slider_0_1)
+        self._variable_slider_0_1_text_box.set_value(self.variable_slider_0_1)
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
+        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
+
+
+def main(top_block_cls=top_block, options=None):
+
+    tb = top_block_cls()
+    tb.Start(True)
+    tb.Wait()
+
+
+if __name__ == '__main__':
+    main()
